099a54a35664f4436ff1d59c1393b2785393e8cc
Vasiliy Nikishin <van@cyberprotect.ru>
2022-12-14 18:59:34 +0300

    HW1 is completed

--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bufio"
+	"context"
+	"fmt"
 	"io"
+	"net"
 	"time"
 )
 
@@ -12,10 +16,62 @@ type TelnetClient interface {
 	Receive() error
 }
 
+type TelnetClientStruct struct {
+	address string
+	timeout time.Duration
+	in      io.ReadCloser
+	out     io.Writer
+	conn    net.Conn
+	ctx     context.Context
+	cancel  context.CancelFunc
+}
+
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+	return &TelnetClientStruct{address: address, timeout: timeout, in: in, out: out}
+}
+
+func (tc *TelnetClientStruct) Connect() error {
+	dialer := &net.Dialer{}
+	tc.ctx, tc.cancel = context.WithTimeout(context.Background(), tc.timeout)
+
+	var err error
+	tc.conn, err = dialer.DialContext(tc.ctx, "tcp", tc.address)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (tc *TelnetClientStruct) Close() error {
+	tc.cancel()
+	if tc.conn != nil {
+		tc.conn.Close()
+	}
+	return nil
+}
+
+func (tc *TelnetClientStruct) Send() error {
+	return tc.scan(bufio.NewScanner(tc.in), tc.conn)
+}
+
+func (tc *TelnetClientStruct) Receive() error {
+	defer tc.cancel()
+	return tc.scan(bufio.NewScanner(tc.conn), tc.out)
+}
+
+func (tc *TelnetClientStruct) scan(scanner *bufio.Scanner, writer io.Writer) error {
+	for {
+		select {
+		case <-tc.ctx.Done():
+			return fmt.Errorf("finished by context done")
+		default:
+			if !scanner.Scan() {
+				return nil
+			}
+			text := scanner.Text()
+			if _, err := writer.Write([]byte(fmt.Sprintf("%s\n", text))); err != nil {
+				return fmt.Errorf("failed to write: %w", err)
+			}
+		}
+	}
+}
