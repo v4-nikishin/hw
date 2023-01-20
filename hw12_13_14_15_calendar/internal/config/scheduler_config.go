6054b17d95cb70c299ef02710bb643fa8a199462
Vasiliy Nikishin <van@cyberprotect.ru>
2023-01-20 19:57:00 +0300

    HW14 is completed

---
+++ hw12_13_14_15_calendar/internal/config/scheduler_config.go
@@ -0,0 +1,42 @@
+package config
+
+import (
+	"os"
+
+	yaml "gopkg.in/yaml.v2"
+)
+
+type SchedulerConfig struct {
+	Logger    LoggerConf    `yaml:"log"`
+	Server    ServerConf    `yaml:"server"`
+	Scheduler SchedulerConf `yaml:"scheduler"`
+	Publisher PublisherConf `yaml:"publisher"`
+}
+
+type SchedulerConf struct {
+	CheckPeriod uint8 `yaml:"checkPeriod"`
+}
+
+type PublisherConf struct {
+	URI          string `yaml:"uri"`
+	Exchange     string `yaml:"exchange"`
+	ExchangeType string `yaml:"exchangeType"`
+	RoutingKey   string `yaml:"routingKey"`
+	Reliable     bool   `yaml:"reliable"`
+}
+
+func NewSchedulerConfig() SchedulerConfig {
+	return SchedulerConfig{}
+}
+
+func LoadSchedulerConfig(filePath string) (SchedulerConfig, error) {
+	cfg := NewSchedulerConfig()
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		return cfg, err
+	}
+	if err = yaml.Unmarshal(data, &cfg); err != nil {
+		return cfg, err
+	}
+	return cfg, err
+}
---
+++ hw12_13_14_15_calendar/internal/config/sebder_config.go
@@ -0,0 +1,37 @@
+package config
+
+import (
+	"os"
+
+	yaml "gopkg.in/yaml.v2"
+)
+
+type SenderConfig struct {
+	Logger   LoggerConf   `yaml:"log"`
+	Consumer ConsumerConf `yaml:"consumer"`
+}
+
+type ConsumerConf struct {
+	URI          string `yaml:"uri"`
+	Exchange     string `yaml:"exchange"`
+	ExchangeType string `yaml:"exchangeType"`
+	Queue        string `yaml:"queue"`
+	BindingKey   string `yaml:"bindingKey"`
+	ConsumerTag  string `yaml:"consumerTag"`
+}
+
+func NewSenderConfig() SenderConfig {
+	return SenderConfig{}
+}
+
+func LoadSenderConfig(filePath string) (SenderConfig, error) {
+	cfg := NewSenderConfig()
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		return cfg, err
+	}
+	if err = yaml.Unmarshal(data, &cfg); err != nil {
+		return cfg, err
+	}
+	return cfg, err
+}
